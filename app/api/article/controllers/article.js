d111d470bb8de374db1ee5e3ea90330e6d573f3e
Colque <colque@gmail.com>
2020-05-15 10:32:12 -0300

    Feature/revocation concluída e testada.

--- app/api/article/controllers/article.js
+++ app/api/article/controllers/article.js
@@ -1,8 +1,95 @@
-'use strict';
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
 
-module.exports = {};
+  async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      data.author = ctx.state.user.id;
+      entity = await strapi.services.article.create(data, { files });
+    } else {
+      ctx.request.body.author = ctx.state.user.id;
+      entity = await strapi.services.article.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.article });
+  },
+
+  /**
+   * Update a record.
+   *
+   * @return {Object}
+   */
+
+  async update(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+
+    const [article] = await strapi.services.article.find({
+      id: ctx.params.id,
+      'author.id': ctx.state.user.id,
+    });
+
+    if (!article) {
+      return ctx.unauthorized(`Vocẽ não pode alterar esse Artigo`);
+    }
+
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.article.update({ id }, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.article.update({ id }, ctx.request.body);
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.article });
+  },
+
+  /**
+   * Delete a record.
+   *
+   * @return {Object}
+   */
+
+  async delete(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+
+    let article;
+    
+    if (ctx.state.user.role.name === 'Admin') {
+      article = await strapi.services.article.find({
+        id: ctx.params.id
+      });
+    } else {
+      article = await strapi.services.article.find({
+        id: ctx.params.id,
+        'author.id': ctx.state.user.id,
+      });
+    }
+
+    if (!article) {
+      return ctx.unauthorized(`Vocẽ não pode deletar esse Artigo`);
+    }
+
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.article.delete({ id }, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.article.delete({ id }, ctx.request.body);
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.article });
+  },  
+
+};
